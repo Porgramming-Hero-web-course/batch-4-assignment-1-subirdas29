1cbf3dbcbc203517aed00c0ec3b6db170c62fe25
subirdas29 <subirdas1045@gmail.com>
2024-11-07 19:54:17 +0600

    problem-3.ts added

---
+++ Assignments/problem-3.ts
@@ -0,0 +1,25 @@
+{
+
+    //problem-3
+
+    
+    const countWordOccurrences = (sentence:string, word:string): number =>{
+        const str = sentence.split(' ')
+        
+        let count = 0;
+
+        str.forEach(ele=>{
+           if(ele === word){
+            count++;
+           }
+        })
+        return count
+    }
+
+    const sentence :string = "I love typescript"
+    const word: string = "typescript"
+
+    const countNum = countWordOccurrences(sentence,word)
+    console.log(countNum)
+
+}
